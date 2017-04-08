aa1eed6da6042ab8457e28c627d7c0d75e3b7d8e
romcheck <romcheck@gmail.com>
2017-04-08 10:25:31 +0200

    remove dwm rules

--- dwm/config.h
+++ dwm/config.h
@@ -21,7 +21,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "chromium", NULL,       NULL,       0,            0,           -1 },
+	{  NULL,      NULL,       NULL,       0,            False,       -1 },
 };
 
 /* layout(s) */
